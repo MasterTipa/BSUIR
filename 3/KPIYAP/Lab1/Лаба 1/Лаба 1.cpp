94d1417c4c90702b9a7f04070c3f5afda73199dd
MasterTipa <71173344+MasterTipa@users.noreply.github.com>
2021-11-17 23:44:10 +0300

    Add files via upload

---
+++ 3/KPIYAP/Lab1/Лаба 1/Лаба 1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	cout << "Enter the text"<<endl;
+	string s;
+	getline(cin,s);
+	cout <<"It's your text : "
+		<< s <<endl<<"His length : "
+		<< s.length()<<endl
+		<<"Reverse text : ";
+	for (int i = s.length();i > 0;i--)
+		cout << s[i - 1];
+	cout << endl<<hex<<25039751;
+
+}
+
