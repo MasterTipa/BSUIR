8a840b4051dbd4a8ede9f5d6fa3d5e9c5e807fdc
MasterTipa <71173344+MasterTipa@users.noreply.github.com>
2021-11-18 00:34:18 +0300

    Add files via upload

---
+++ 1/SomeLabs/Задача2,1/Задача2,1/Задача2,1.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+//13
+
+
+int main()
+{	setlocale(LC_ALL, "Russian");								//Вводим руссификацию
+int a,b=0;														//Вводим переменные
+	do
+{
+		while (!scanf_s("%d", &a))								//Проверяем на правильность ввода данных 
+		{
+			printf("Не корректный ввод, повторите попытку:");
+			rewind(stdin);
+		};
+	if (a > 0) b += a;											//Производим счёт данных
+} while (a != 0);
+printf("Сумма элементов больше 0 = %d", b);
+
+	return 0;
+}
---
+++ 1/SomeLabs/Задача2,2/Задача 2,2/Задача 2,2.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+//13
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");										//Вводим руссификацию
+	int k,n;
+	double s=1;
+		while (!scanf_s("%d", &n))										//Проверяем на правильность ввода данных 
+		{
+			printf("Не корректный ввод переменной n, повторите попытку:");
+			rewind(stdin);
+		};
+		while (!scanf_s("%d", &k))										//Проверяем на правильность ввода данных 
+		{
+			printf("Не корректный ввод переменной k, повторите попытку:");
+			rewind(stdin);
+		};
+		for (int i=2;i<=k;i++)
+		{
+			s += 1.0 / (i * n);
+		}
+		
+		
+		printf("\nСумма всех элементов последовательности:%f", s);
+		
+
+		
+	return 0;
+}
